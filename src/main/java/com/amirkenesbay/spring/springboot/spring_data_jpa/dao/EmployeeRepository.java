7fb2e424ae48c73096b89e77c24385a66235e768
amirkenesbay <amirkenesbay@gmail.com>
2022-02-22 18:41:45 +0600

    Initial commit

---
+++ src/main/java/com/amirkenesbay/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.amirkenesbay.spring.springboot.spring_data_jpa.dao;
+
+import com.amirkenesbay.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
---
+++ src/main/java/com/amirkenesbay/spring/springboot/spring_data_jpa/service/EmployeeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.amirkenesbay.spring.springboot.spring_data_jpa.service;
+
+import com.amirkenesbay.spring.springboot.spring_data_jpa.dao.EmployeeRepository;
+import com.amirkenesbay.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Employee employee = null;
+        Optional<Employee> optional = employeeRepository.findById(id);
+        if (optional.isPresent()) {
+            employee = optional.get();
+        }
+        return employee;
+    }
+}
